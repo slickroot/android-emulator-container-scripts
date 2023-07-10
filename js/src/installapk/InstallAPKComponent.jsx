d2003584e652173acdc16c860d4d49ed3ec6a033
Marouane Elaich <elaich.marouane@gmail.com>
2023-07-10 15:10:53 +0100

    Added JS component to install an APK from URL

---
+++ js/src/installapk/InstallAPKComponent.jsx
@@ -0,0 +1,44 @@
+import { useState, useCallback } from "react";
+import TextField from "@mui/material/TextField";
+import IconButton from "@mui/material/IconButton";
+import InsatllMobileIcon from "@mui/icons-material/InstallMobile";
+import { installAPKFromURL } from "./apkInstaller";
+
+const InstallAPKComponent = () => {
+  const [url, setUrl] = useState("")
+  const [loading, setLoading] = useState(false)
+
+  const handleInstallAPK = useCallback(async () => {
+    setLoading(true)
+    installAPKFromURL(url)
+      .then(() => {
+        setUrl("")
+        setLoading(false)
+      })
+  }, [url])
+
+  return (
+    <div style={{ display: "flex" }}>
+      <TextField 
+        fullWidth
+        sx={{
+          backgroundColor: 'white'
+        }}
+        label="APK URL" 
+        value={url} 
+        onChange={e => setUrl(e.target.value)}
+      />
+      <IconButton
+        aria-label="Install APK from Link"
+        color="inherit"
+        onClick={handleInstallAPK}
+        size="large"
+        disabled={loading}
+      >
+        <InsatllMobileIcon />
+      </IconButton>
+    </div>
+  )
+}
+
+export default InstallAPKComponent
